04439fedf44b78c7d7ebf1b5c7ae7cb7f7a20ca5
Dalerjons <dn382@student.aru.ac.uk>
2025-08-15 13:06:04 +0100

    added new route for categories

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 # main master
 #  pull request
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(
+    title='My API',
+    description=' My first API'
+)
 
 
+class CategorySchema(BaseModel):
+    id: int
+    name: str
 
 
+@app.get('/api/categories')
+def get_categories() -> list[CategorySchema]:
+    categories = [CategorySchema(id=i, name=f'category-{i}')for i in range(1, 11)]
+    return categories
